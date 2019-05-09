38a840a8c949af71bd39dcd6c8df4e9b766ea98d
Zharfan Mazli <zhaff@neptunescripts.com>
2019-05-09 10:10:46 +0800

    Add instructions and the script file

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Powershell-SharePoint-Access-Checker
 Powershell script to generate all access permissions in a given SharePoint site.
+
+## Step to run the script
+
+1) Unzip the zip file to folder "C:\sp-checker"
+
+2) Install PowerShell SharePoint module accoding to your SharePoint version here: 
+https://github.com/sharepoint/pnp-powershell/releases
+
+3) Open file "sp-checker.ps1" using Notepad
+
+4) Modify the existing settings
+
+```sh
+# Enter your username here
+$Username = "username"
+
+# Enter your password here 
+$Password = "password"
+
+# Enter folder path to save the logs files 
+$LogsFolder = "C:\sp-checker\logs" 
+
+# Enter excel file path that contains the URLs
+$URLsFile = "C:\sp-checker\SiteCollectionList.csv" 
+```
+
+4) Save
+
+5) Right-click the file "get-sp-access.ps1" and click Run with PowerShell
+
+* Enter 'Y' if it asks for Execution policy change
+
+* If you need to stop it, press CTRL + C
+
+6) All logs files can be found in folder "C:\sp-checker\logs"
+
+* You may update URLs to execute in excel file: "SiteCollectionList.csv" using Notepad
+
---
+++ sp-checker.ps1
@@ -0,0 +1,86 @@
+﻿#region Settings - UPDATE YOUR SETTINGS HERE
+
+# Enter your username here
+$Username = "username"
+
+# Enter your password here 
+$Password = "password"
+
+# Enter folder to save the logs files 
+$LogsFolder = "C:\sp-checker\logs" 
+
+# Enter excel file that contains the URLs
+$URLsFile = "C:\sp-checker\SiteCollectionList.csv" 
+
+#endregion Variables
+
+#region Credentials 
+[SecureString]$SecurePass = ConvertTo-SecureString $Password -AsPlainText -Force 
+[System.Management.Automation.PSCredential]$PSCredentials = New-Object System.Management.Automation.PSCredential($Username, $SecurePass) 
+#endregion Credentials
+
+$siteUrls = New-Object System.Collections.ArrayList
+Import-Csv "$URLsFile" |
+ForEach-Object {
+    $siteUrls.Add($_.SiteUrl);
+}
+
+$dt = Get-Date
+
+foreach($siteUrl in $siteUrls){
+
+    $filename = $siteUrl.replace("/","_").replace(":","_")
+
+    Start-Transcript -Path "$LogsFolder\$filename.txt"
+
+    write-host
+
+    write-host "DateTime: $dt.ToUniversalTime()"
+    write-host "URL: $siteUrl"
+    write-host
+
+    #$cred = get-credential
+    Connect-PnPOnline -Url $siteUrl -Credentials $PSCredentials
+
+
+    $web = Get-PnPWeb -Includes RoleAssignments
+
+    write-host	"#################################"
+    write-host	"## Site Permission List:"
+    write-host	"#################################"
+    write-host
+
+    $no = 1
+
+    foreach($ra in $web.RoleAssignments) {
+        $member = $ra.Member
+        $loginName = get-pnpproperty -ClientObject $member -Property LoginName
+        $rolebindings = get-pnpproperty -ClientObject $ra -Property RoleDefinitionBindings
+    
+        write-host "$no. $($loginName) - $($rolebindings.Name)"
+
+        $no = $no + 1;
+
+        #write-host $members
+
+        $members = get-pnpproperty -ClientObject $ra -Property Member
+        
+        if ($($ra.Member.PrincipalType)) {
+
+            if ($($ra.Member.PrincipalType.ToString()) -eq "SharePointGroup") {
+                write-host
+                Get-PnPGroupMembers -Identity "$loginName"
+                write-host
+           }
+        }
+
+    }
+
+    write-host	"#################################"
+    write-host	"## Site Collection Admin:"
+    write-host	"#################################"
+    write-host
+    Get-PnPSiteCollectionAdmin
+
+    Stop-Transcript
+}
